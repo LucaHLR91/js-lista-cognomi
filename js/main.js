3f91d6952d48e645b12e0fb7a96893eff7f908aa
Luca <mastrantonio.luca.91@icloud.com>
2021-07-20 15:50:58 +0200

    Complete

--- js/main.js
+++ js/main.js
@@ -9,6 +9,11 @@ var list = ['Bianchi', 'Rossi', 'Neri', 'Gialli', 'Verdi'];
 
 // chiedi il cognome all'utente e inseriscilo nella lista
 var utente = prompt('inserisci il tuo cognome');
-list.push = utente;
+list.push(utente);
 
 console.log(list);
+console.log(list.sort());
+
+var ordered_list = list.sort();
+// scrivi la posizione umana della lista in cui il nuovo utente si trova
+console.log(ordered_list.indexOf(utente) + 1);
