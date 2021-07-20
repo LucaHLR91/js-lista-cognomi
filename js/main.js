ec443799129b5ae3cf8b5e5b9675d06ff9725b54
Luca <mastrantonio.luca.91@icloud.com>
2021-07-20 14:16:59 +0200

    User data + unordered surname list

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@ Consegna:
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 // 3. stampa la lista ordinata alfabeticamente
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+// array con altri cognomi
+var list = ['Bianchi', 'Rossi', 'Neri', 'Gialli', 'Verdi'];
+
+// chiedi il cognome all'utente e inseriscilo nella lista
+var utente = prompt('inserisci il tuo cognome');
+list.push = utente;
+
+console.log(list);
