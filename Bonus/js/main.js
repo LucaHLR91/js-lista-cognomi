bae9c57456be01755c3c34677f8b5d1c1beceee4
Luca <mastrantonio.luca.91@icloud.com>
2021-07-20 20:55:29 +0200

    Bonus folder

---
+++ Bonus/js/main.js
@@ -0,0 +1,20 @@
+Consegna:
+// 1. chiedi all’utente il cognome
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// 3. stampa la lista ordinata alfabeticamente
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+// array con altri cognomi
+var list = ['Bianchi', 'Rossi', 'Neri', 'Gialli', 'Verdi'];
+
+// chiedi il cognome all'utente e inseriscilo nella lista
+var utente = prompt('inserisci il tuo cognome');
+utente = utente.charAt(0).toUpperCase() + utente.slice(1).toLowerCase();
+list.push(utente);
+
+console.log(list);
+console.log(list.sort());
+
+// scrivi la posizione umana della lista in cui il nuovo utente si trova
+var ordered_list = list.sort();
+console.log(ordered_list.indexOf(utente) + 1);
